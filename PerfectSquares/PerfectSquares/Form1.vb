ca433f6dfa65ae628e9571f1c48029059fbf9a9b
alex diersen <Dier0128>
2013-10-09 19:51:03 -0500

    forgot to submit earlier

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,16 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
-
+        Dim numb As Integer = 1
+        Dim perfSquare As Integer
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
 
+        Do Until numb > 10
+            perfSquare = numb ^ 2
+
+            lstResult.Items.Add(numb & "*" & numb & "n" & perfSquare)
+            numb = 1
+        Loop
+        
     End Sub
 End Class
